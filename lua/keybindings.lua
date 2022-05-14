19672bd53e0c3b920be4478822a023a5c4ab36c1
doupongzeng <huangzeng2016@126.com>
2022-05-14 21:23:13 +0800

    feat: add key configure file

---
+++ lua/keybindings.lua
@@ -0,0 +1,68 @@
+--[[
+-- vim.api.nvim_set_keymap() 全局快捷键
+-- vim.api.nvim_buf_set_keymap() buffer快捷键
+-- vim.api.nvim_set_keymap('模式', '按键', '映射为', 'options')
+-- n Normal, i Insert, v Visual, t Terminal, c Command
+-- options: noremap 不会连续映射，silent 不输出多余信息
+--]]
+
+-- remap <leader>
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local map = vim.api.nvim_set_keymap
+local opt = {noremap = true, silent = true}
+
+------------------------------
+--windows 分屏
+------------------------------
+-- 取消s默认功能，映射分屏
+map('n', 's', "", opt)
+map('n', 'sv', ':vsp<CR>', opt)
+map('n', 'sh', ':sp<CR>', opt)
+-- 关闭当前
+map('n', 'sc', ':<C-w>c', opt)
+-- 关闭其它
+map('n', 'so', ':<C-w>o', opt)
+-- Alt + hjkl 窗口之间跳转
+map('n', '<A-h>', '<C-w>h', opt)
+map('n', '<A-j>', '<C-w>j', opt)
+map('n', '<A-k>', '<C-w>k', opt)
+map('n', '<A-l>', '<C-w>l', opt)
+-- 上下左右比例控制
+map('n', '<C-Left>', ':vertical resize -2<CR>', opt)
+map('n', '<C-Right>', ':vertical resize +2<CR>', opt)
+map('n', 's,', ':vertical resize -20<CR>', opt)
+map('n', 's.', ':vertical resize +20<CR>', opt)
+map('n', '<C-Down>', ':resize +2<CR>', opt)
+map('n', '<C-Up>', ':resize -2<CR>', opt)
+map('n', 'sj', ':resize +10<CR>', opt)
+map('n', 'sk', ':resize -10<CR>', opt)
+map('n', 's=', '<C-w>=', opt)
+-- Terminal 分屏
+map("n", "<leader>t", ":sp | terminal<CR>", opt)
+map("n", "<leader>vt", ":vsp | terminal<CR>", opt)
+map("t", "<Esc>", "<C-\\><C-n>", opt)
+map("t", "<A-h>", [[ <C-\><C-N><C-w>h ]], opt)
+map("t", "<A-j>", [[ <C-\><C-N><C-w>j ]], opt)
+map("t", "<A-k>", [[ <C-\><C-N><C-w>k ]], opt)
+map("t", "<A-l>", [[ <C-\><C-N><C-w>l ]], opt)
+------------------------------
+
+--Visual
+map('v', '<', '<gv', opt)
+map('v', '>', '>gv', opt)
+-- 上下移动[选中]的文本
+map('v', 'J', ":move '>+1<CR>gv-gv", opt)
+map('v', 'K', ":move '<-2<CR>gv-gv", opt)
+
+-- 快速移动
+map('n', '<C-j>', '5j', opt)
+map('n', '<C-k>', '5k', opt)
+-- ctrl uppage, ctrl downpage, 默认半屏
+-- map('n', '<C-u>', '9k', opt)
+-- map('n', '<C-d>', '9j', opt)
+
+-- 在visul模式里粘贴不要复制
+map('v', 'p', '"_dP', opt)
+
