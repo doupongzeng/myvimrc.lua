c354f6f6a6f7de8c2f362e0e6c2a1653a2e58de2
doupongzeng <huangzeng2016@126.com>
2022-05-16 13:06:51 +0800

    feat: packer auto install

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,36 @@
-local packer = require('packer')
+-- 自动安装 Packer.nvim
+-- 插件安装目录
+-- ~/.local/share/nvim/site/pack/packer/
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local paccker_bootstrap
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.notify("Installing Pakcer.nvim，wait...")
+  paccker_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    -- "https://gitcode.net/mirrors/wbthomason/packer.nvim",
+    install_path,
+  })
+
+  -- https://github.com/wbthomason/packer.nvim/issues/750
+  local rtp_addition = vim.fn.stdpath("data") .. "/site/pack/*/start/*"
+  if not string.find(vim.o.runtimepath, rtp_addition) then
+    vim.o.runtimepath = rtp_addition .. "," .. vim.o.runtimepath
+  end
+  vim.notify("Pakcer.nvim compelete")
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  vim.notify("not found packer.nvim")
+  return
+end
+
 packer.startup({
   function(use)
     -- :h base-directories
