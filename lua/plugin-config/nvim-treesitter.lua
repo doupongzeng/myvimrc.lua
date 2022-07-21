228e5a17e295b1736dbad4ae8cccd1c3d7ba17f6
doupongzeng <huangzeng2016@email.com>
2022-07-21 15:14:14 +0800

    feat: add python

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -7,7 +7,7 @@ end
 treesitter.setup({
   -- 安装 language parser
   -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = { "json", "html", "css", "vim", "lua", "javascript", "typescript", "tsx", "cpp", "bash", "cuda" },
+  ensure_installed = { "json", "html", "css", "vim", "lua", "javascript", "typescript", "tsx", "cpp", "bash", "cuda", "python" },
   -- 启用代码高亮模块
   highlight = {
     -- 开启高亮
