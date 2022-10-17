84628e8c7fdc83b9b06629846eb47c72ceee3da0
doupongzeng <huangzeng2016@email.com>
2022-10-17 12:28:50 +0800

    fix: lsp api update

--- lua/lsp/config/json.lua
+++ lua/lsp/config/json.lua
@@ -4,7 +4,7 @@ local opts = {
       schemas = require("schemastore").json.schemas(),
     },
   },
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
   flags = {
     debounce_text_changes = 150,
   },
--- lua/lsp/config/python.lua
+++ lua/lsp/config/python.lua
@@ -8,7 +8,7 @@ local opts = {
       }
     }
   },
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
   flags = {
     debounce_text_changes = 150,
   },
