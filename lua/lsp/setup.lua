1a83cfe8451f535cf8d307afa717c98f988e7137
doupongzeng <huangzeng2016@126.com>
2023-02-14 16:10:09 +0800

    feat: update to lua_ls

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -7,7 +7,7 @@ local lspconfig = require('lspconfig')
 -- key 必须为下列网址列出的名称
 -- https://github.com/williamboman/nvim-lsp-installer#available-lsps
 local servers = {
-  sumneko_lua = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
+  lua_ls = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
   bashls = require('lsp.config.bash'),
   cmake = require('lsp.config.cmake'),
   pyright = require('lsp.config.python'),
